9f7a2199752ee88f27d8d880f76009c831e76390
SHIVAM KUMAR KASAUDHAN <124242999+shivamkasaudhan@users.noreply.github.com>
2023-12-10 00:32:27 +0530

    Modified some basic css and java script

--- js/index.js
+++ js/index.js
@@ -1,20 +1,23 @@
 
 
-submitBTN.addEventListener("click", async(e) => {
+submitBTN.addEventListener("click", async (e) => {
     e.preventDefault()
     console.log("clicked")
-    Results.innerHTML =`<img width="23" src="img/loading.svg" alt="" >`
+    Results.innerHTML = `<img width="23" src="img/loading.svg" alt="" >`
     let key = "ema_live_kJGCL9PIfyXZqocrEqxQL99ZL4OA4HFVavCSMsSG"
     let email = document.getElementById("username").value
     let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
     let res = await fetch(url)
     let result = await res.json()
-    str = ``
-    for (key of Object.keys(result)) {
-        if (result[key]!=="" && result[key]!==" ") {
-            str = str + `<div>${key}:${result[key]}</div>`
+    let str = `<table>`;
+    for (const key of Object.keys(result)) {
+        if (result[key] !== "" && result[key] !== " ") {
+            const formattedKey = key.charAt(0).toUpperCase() + key.slice(1);
+            str += `<tr><td><b>${formattedKey}</b></td><td>${result[key]}</td></tr>`;
         }
     }
+    str += `</table>`;
+
     console.log(str)
     Results.innerHTML = str
 
